4aadb036c11e55e179025d48834e5229495f3881
Lucas Schwartzentruber <klucas.schwartz@gmail.com>
2022-09-10 09:08:16 -0400

    fix misnamed dag arg schedule

--- airflow/dags/example_xcomargs.py
+++ airflow/dags/example_xcomargs.py
@@ -35,7 +35,7 @@ with DAG(
     "example_xcom_args_with_operators",
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
-    schedule=None,
+    schedule_interval=None,
     tags=['example'],
 ) as dag2:
     bash_op1 = BashOperator(task_id="c", bash_command="echo c")
