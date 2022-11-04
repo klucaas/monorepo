b2ae6e0ad1f93a11a2e57a1a27d9937ecd336d49
Lucas Schwartzentruber <klucas.schwartz@gmail.com>
2022-11-03 21:09:48 -0400

    ADD LOGGING

--- airflow/dags/lululemon_we_made_to_much.py
+++ airflow/dags/lululemon_we_made_to_much.py
@@ -135,7 +135,7 @@ def taskflow():
         TWILIO_NUMBER = Secret(deploy_type="env", deploy_target="TWILIO_NUMBER", secret="airflow-secrets",
                                  key="TWILIO_NUMBER")
 
-        logging.info(f"{exit_criteria}")
+        logging.info(f"EXIT CRITERIA: {exit_criteria}")
 
         client = Client()
         #client.messages.create(body="this is a test message", from_=os.environ["TWILIO_NUMBER"], to=os.environ["TEXT_RECIPIENT"])
